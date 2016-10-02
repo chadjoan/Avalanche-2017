db864858a7fc3981abf115b3b6bdc867b005c267
Pandemic Moon <pandemic@moon-network.net>
2016-10-02 11:28:42 -0400

    Fixed KeithFirstTeleOp

--- Avalanche/src/main/java/org/firstinspires/ftc/avalanche/teleop/KeithFirstTeleOp.java
+++ Avalanche/src/main/java/org/firstinspires/ftc/avalanche/teleop/KeithFirstTeleOp.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "First Teleop", group = "Education")
+@TeleOp(name = "Keith's First Teleop", group = "Education")
 public class KeithFirstTeleOp extends LinearOpMode {
 
     private DcMotor motorLeftFront;
